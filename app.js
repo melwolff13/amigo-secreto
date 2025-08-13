8d02996eca92bbcae5bd1d5f4ec367714da89a81
Melissa Wolff <melmendeswr0213@gmail.com>
2025-08-12 22:50:46 -0300

    Função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ function adicionarAmigo() {
         alert('Por favor, insira um nome.');
     } else { 
         amigos.push(amigo);
-    }
+        let listaAmigos = document.getElementById('listaAmigos'); 
+        listaAmigos.innerHTML = '';
+        // executa a função atualizarLista para cada elemento de amigos
+        amigos.forEach(amigo => atualizarLista(amigo));
+    } limparCampo();
+}
+
+// adiciona o nome do amigo à lista visível do usuário
+function atualizarLista(amigo) {
+    let novoAmigo = document.createElement('li');
+    novoAmigo.textContent = amigo;
+    listaAmigos.appendChild(novoAmigo);
+}
+
+function limparCampo() {
+    let amigo = document.querySelector('input');
+    amigo.value = '';
 }

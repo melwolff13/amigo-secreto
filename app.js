215c0f8f5c6307bf787a8b8a7725a7445677739a
Melissa Wolff <melmendeswr0213@gmail.com>
2025-08-12 22:43:32 -0300

    Função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.querySelector('input').value;
+    
+    if (amigo == '') { 
+        alert('Por favor, insira um nome.');
+    } else { 
+        amigos.push(amigo);
+    }
+}
